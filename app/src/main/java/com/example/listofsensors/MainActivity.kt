76a9e779309f84f8f39a3badb9a50f04200966b7
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2025-02-06 22:57:46 +0200

    List of available sensors. Completed

--- app/src/main/java/com/example/listofsensors/MainActivity.kt
+++ app/src/main/java/com/example/listofsensors/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.listofsensors
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,15 +11,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 import com.example.listofsensors.ui.theme.ListOfSensorsTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,11 +31,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ListOfSensorsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ThemeDemo(
-
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ListOfSensors()
                 }
             }
         }
@@ -37,15 +43,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ThemeDemo(modifier: Modifier) {
-    Column(
-    modifier = modifier
-    )  {
+fun ListOfSensors() {
+    val ctx = LocalContext.current
+    val sensorManager: SensorManager = ctx.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    val deviceSensors: List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
+
+    Column {
         Text(
             text = "Available sensors",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(32.dp).fillMaxWidth()
         )
+        LazyColumn {
+            deviceSensors.forEach{ sensor ->
+                item {
+                    HorizontalDivider()
+                    Text (
+                        text = sensor.name,
+                        modifier = Modifier.padding(8.dp)
+                    ) }
+
+            }
+        }
     }
-}
+
+    }
